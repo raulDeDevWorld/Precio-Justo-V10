eafbec4dbefdab3d461bd9c747967eeb071580c1
Your Name <you@example.com>
2023-05-29 21:45:22 +0200

    DC

--- src/app/(with-layout)/layout.jsx
+++ src/app/(with-layout)/layout.jsx
@@ -28,7 +28,7 @@ export default function Home({ children }) {
                 <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Search icon</span>
               </div>
-              <input type="text" id="search-navbar" class="block w-full p-2 pl-10 text-sm text-gray-900 border-b border-gray-300  bg-transparent focus:ring-blue-500 focus:border-blue-500 " placeholder="Busca tu producto..." />
+              <input type="text" id="search-navbar" class="block w-full p-2 pl-10 text-sm text-gray-200 border-b border-gray-300  bg-transparent focus:ring-white focus:border-white focus:outline-transparent" placeholder="Busca tu producto..." />
             </div>
 
             <button type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={() => setNav(!nav)}>
